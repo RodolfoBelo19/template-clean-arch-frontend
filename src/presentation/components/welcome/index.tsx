81943e18301c509b85cb11717b8709f41ad58ac9
Rodolfo Belo <64740047+RodolfoBelo19@users.noreply.github.com>
2023-03-15 18:37:53 -0300

    feat: clean arch frontend

---
+++ src/presentation/components/welcome/index.tsx
@@ -0,0 +1,11 @@
+export const Welcome = () => {
+  return (
+    <div className="flex flex-col p-5 justify-center items-center h-screen bg-zinc-900 text-white">
+      <h1 className="text-center">Welcome to template nextjs with typescript and tailwindcss</h1>
+      <small className="py-2">with</small>
+      <h4>Clean Arch</h4>
+
+      <p className="mt-12">by: rodolfo belo</p>
+    </div>
+  )
+}
